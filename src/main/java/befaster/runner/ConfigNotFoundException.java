d67c9ae9f3003150b8066340e079605d7c7a0e6f
sbx_user1059 <sbx_user1059@ip-10-128-143-124.eu-west-2.compute.internal>
2019-03-05 07:27:10 +0000

    Tue Mar 05 07:27:10 UTC 2019

--- src/main/java/befaster/runner/ConfigNotFoundException.java
+++ src/main/java/befaster/runner/ConfigNotFoundException.java
@@ -1,8 +1,11 @@
 package befaster.runner;
 
+import java.util.*
+
 public class ConfigNotFoundException extends Exception {
     ConfigNotFoundException(String message) {
         super(message);
     }
 
 }
+
